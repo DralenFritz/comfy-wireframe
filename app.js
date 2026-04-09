f658bfeface8044a79a66a291c2d567c2a093660
Dralen <oginnisayofunmi12@gmail.com>
2026-04-09 23:55:57 +0100

    absolute path to send the public folder

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const path = require('path');
 
 const mainRouter = require('./routes/main');
 const notFoundMiddleware = require("./middleware/not-found");
@@ -8,7 +9,7 @@ const notFoundMiddleware = require("./middleware/not-found");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static('./public'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', mainRouter);
 
